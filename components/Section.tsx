b9ba2a66d044ca5837961e87a17ffbad132ba813
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2026-02-15 12:59:27 +0330

    added: margin to section

--- components/Section.tsx
+++ components/Section.tsx
@@ -6,7 +6,7 @@ interface SectionProps {
 
 export default function Section({ children, className = '', id }: SectionProps) {
   return (
-    <section id={id} className={`mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-16 md:py-24 ${className}`}>
+    <section id={id} className={`mx-1 max-w-7xl px-4 sm:px-6 sm:mx-4 lg:px-8 py-16 md:py-24 rounded-2xl my-4 bg-background/95 backdrop-blur ${className}`}>
       {children}
     </section>
   )
