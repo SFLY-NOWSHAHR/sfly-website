f7e96dd202a2162dc3571fcac68e545b088ff540
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2026-02-25 20:11:31 +0330

    modified cta component

--- components/Cta.tsx
+++ components/Cta.tsx
@@ -38,7 +38,7 @@ export default function CTA ({imgSrc, title, desc, href, linkContent}:CtaType){
                     <Link
                         href={`/${href}`}
                         className="inline-flex items-center justify-center px-6 py-3 bg-primary text-white font-bold rounded-lg hover:bg-secondary transition-all">
-                        → {linkContent} 
+                        {linkContent} ⭠ 
                     </Link>
                 </div>
             </Section>
